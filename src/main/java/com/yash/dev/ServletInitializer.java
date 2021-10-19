67fc19673edaea0800a36aa2760d236ef4ec6d73
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-10-19 09:11:44 +0530

    Commit with Backend Receipe Endpoints

--- src/main/java/com/yash/dev/ServletInitializer.java
+++ src/main/java/com/yash/dev/ServletInitializer.java
@@ -3,6 +3,10 @@ package com.yash.dev;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Created by Yashwanth Anand S
+ **/
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
