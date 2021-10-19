d22bac44545de5e662dfe5350b8ccba67d5ec8ca
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-10-19 09:02:30 +0530

    Commit with Initial Project

---
+++ src/main/java/com/yash/dev/BackendReceipeApiApplication.java
@@ -0,0 +1,13 @@
+package com.yash.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReceipeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendReceipeApiApplication.class, args);
+	}
+
+}
